a184db9a0f5ed2f351c1004143a38d1dd26460b3
Mo <godot83@gmail.com>
2019-01-04 14:50:33 +0100

    ok

--- main.tf
+++ main.tf
@@ -7,8 +7,25 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "${var.AMI}"
     instance_type = "${var.INSTANCE_TYPE}"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+    user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello, World" > index.html
+    nohup busybox httpd -f -p 8080 &
+    EOF
 
     tags {
     Name = "${var.NAME}"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
     }
 }
